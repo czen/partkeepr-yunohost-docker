7be3150073f5b799118fc262d0d5de72a888741f
Баглий Антон Павлович <bagliy_ap@studzone.local>
2019-01-04 22:30:18 +0300

    TRY using partkeepr docker image with port number in environment var

--- scripts/docker/run.sh
+++ scripts/docker/run.sh
@@ -5,12 +5,13 @@
 #[ "$architecture" == "armhf" ] && mhubig/partkeepr:latest
 [ -z $image ] && ynh_die "Sorry, your $architecture architecture is not supported ..."
 
-options="-p $port:80 -v $data_path/data:/data -v /var/run/docker.sock:/var/run/docker.sock"
+# options="-p $port:80 -v $data_path/data:/data -v /var/run/docker.sock:/var/run/docker.sock"
+options="-v $data_path/data:/data -v /var/run/docker.sock:/var/run/docker.sock"
 containeroptions="--no-auth"
 
 iptables -t filter -N DOCKER
 
-docker run -d --network="host" --name=$app --restart always $options $image $containeroptions 1>&2
+docker run -d --network="host" --name=$app --restart always $options -e PORT=$port $image $containeroptions 1>&2
 CR=$?
 
 echo $CR
